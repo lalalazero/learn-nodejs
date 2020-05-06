fe1159b19399bf8c2c3ae6150fb9cb992c951846
lalalazero <zzzero520@hotmail.com>
2020-05-06 16:47:16 +0800

    req.query

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res) {
+    if (req.query.user) {
+        var user = req.query.user 
+        res.send('your query is' + user)
+        return
+    }
     res.send('Hello Express')
 })
+
 
 app.listen(3000, function() {
     console.log('App is listening at port 3000')
