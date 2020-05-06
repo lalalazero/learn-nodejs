4b46dad48ac1d14e3cfa7afb7a04e5c79e117f46
lalalazero <zzzero520@hotmail.com>
2020-05-06 18:14:48 +0800

    superagent + cheerio 爬取 cnode 文章

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var superagent = require('superagent')
+var cheerio = require('cheerio')
 var app = express()
 var multer = require('multer') // for parsing multipart/form-data
 var upload = multer({ dest: 'uploads/' })
@@ -13,6 +15,26 @@ app.get('/', function(req, res) {
         return
     }
     res.send('Hello Express')
+})
+
+app.get('/crawler', function(req, res, next) {
+    superagent.get('https://cnodejs.org/')
+        .end(function (err, sres) {
+            if (err) {
+                return next(err)
+            }
+            var $ = cheerio.load(sres.text)
+            var items = []
+            $('#topic_list .topic_title').each(function (idx, element) {
+                var $element = $(element)
+                items.push({
+                    title: $element.attr('title'),
+                    href: $element.attr('href')
+                })
+            })
+            res.setHeader('Access-Control-Allow-Origin','*')
+            res.json(items)
+        })
 })
 
 app.post('/profile', upload.none(), function (req, res, next) {
