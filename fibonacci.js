7a6ee8c9a732b0f636c79dfc266839d4a9ee1049
lalalazero <zzzero520@hotmail.com>
2020-05-07 12:41:13 +0800

    mocha should istanbul 测试

---
+++ fibonacci.js
@@ -0,0 +1,25 @@
+var fibonacci = function(n) {
+    if (typeof n !== 'number') {
+        throw new Error('n should be a Number')
+    }
+    if (n < 0) {
+        throw new Error('n should >= 0')
+    }
+    if (n > 10) {
+        throw new Error('n should <= 10')
+    }
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+if (require.main === module) {
+    // 如果直接执行 fibonacci.js 则进入此处，
+    // 如果是被其他 module require, 那么这里不会执行
+    var n = Number(process.argv[2])
+    console.log('fibonacci(' + n + ') is ' + fibonacci(n))
+}
+exports.fibonacci = fibonacci
---
+++ test/fibonacci.test.js
@@ -0,0 +1,34 @@
+var fibonacci = require('../fibonacci').fibonacci 
+var should = require('should')
+
+describe('test/fibonacci.test.js', function() {
+    it('fibonacci(10) should equal 55', function() {
+        fibonacci(10).should.equal(55)
+    })
+
+    it('fibonacci(1) should equal 1', function() {
+        fibonacci(1).should.equal(1)
+    })
+
+    it('fibonacci(0) should equal 0', function() {
+        fibonacci(0).should.equal(0)
+    })
+
+    it('fibonacci should throw error when n > 10', function() {
+        (function(){
+            fibonacci(11)
+        }).should.throw('n should <= 10')
+    })
+
+    it('fibonacci should throw error when n < 0', function() {
+        (function(){
+            fibonacci(-1)
+        }).should.throw('n should >= 0')
+    })
+
+    it('fibonacci should throw error when n isnt a number', function() {
+        (function() {
+            fibonacci('string')
+        }).should.throw('n should be a Number')
+    })
+})
